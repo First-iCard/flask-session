eaf07ffa2ced732fb5bcbe9829331221ee89a083
bsmar <bsmar35@gmail.com>
2016-09-05 15:43:48 +0300

    added atomic transaction for test

--- flask_session/sessions.py
+++ flask_session/sessions.py
@@ -642,8 +642,9 @@ class PeeweeSessionInterface(SessionInterface):
                 return self.session_class(sid=sid, permanent=self.permanent)
 
         store_id = self.key_prefix + sid
-        saved_session = self.sql_session_model.select().where(
-            self.sql_session_model.session_id == store_id).first()
+        with self.db.atomic():
+            saved_session = self.sql_session_model.select().where(
+                self.sql_session_model.session_id == store_id).first()
 
         compare_date = datetime.utcnow() if self.permanent else datetime.now()
         if saved_session and saved_session.expiry <= compare_date:
